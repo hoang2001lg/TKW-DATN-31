e5c4d49bd1c2125d8353775329fb752b858ab60b
hoang2001lg <anhyeuem2001lg@gmail.com>
2022-10-11 15:14:00 +0700

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import AdminLayout from './page/layout/AdminLayout'
+import WebsiteLayout from './page/layout/WebsiteLayout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+        <div className='App'>
+
+          <Routes>
+            {/* clients */}
+            <Route path='/' element={< WebsiteLayout />} >
+            </Route>
+            {/* admin */}
+            <Route path="admin" element={< AdminLayout />} >
+
+            </Route>
+            
+          </Routes>
+        </div>
     </>
   )
 }
